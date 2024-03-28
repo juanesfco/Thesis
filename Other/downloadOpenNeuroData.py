705717e37bc6dc80a711ba24e502014dddf9ecb7
Juan Esteban Florez Coronel <juan.florez@upr.edu>
2024-03-28 04:33:58 -0400

    Delete Other directory

--- Other/downloadOpenNeuroData.py
+++
@@ -1,5 +0,0 @@
-import openneuro as on
-
-def download(ON_AN,path = '/content/drive/MyDrive/UPRM/MS/Thesis/Programming/'):
-  td = path + 'Data/' + ON_AN
-  on.download(dataset=ON_AN, target_dir=td)
